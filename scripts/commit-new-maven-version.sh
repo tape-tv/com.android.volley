780b414560bad67d0d101af71a0db5a6c44f80c6
chdorner <christof@chdorner.com>
2015-08-20 18:08:24 +0200

    Add upstream version/type/repository to commit message

--- scripts/commit-new-maven-version.sh
+++ scripts/commit-new-maven-version.sh
@@ -2,6 +2,9 @@
 
 maven_repository_directory=$1
 aar_version=$2
+upstream_version=$3
+upstream_type=$4
+upstream_repository=$5
 
 if [ -z "$maven_repository_directory" ]; then
   echo "No maven repository directory defined"
@@ -13,6 +16,21 @@ if [ -z "$aar_version" ]; then
   exit 1
 fi
 
+if [ -z "$upstream_version" ]; then
+  echo "No upstream version defined"
+  exit 1
+fi
+
+if [ -z "$upstream_type" ]; then
+  echo "No upstream type defined"
+  exit 1
+fi
+
+if [ -z "$upstream_repository" ]; then
+  echo "No upstream repository defined"
+  exit 1
+fi
+
 cd $maven_repository_directory
 git add .
-git commit -m "Add version $aar_version"
+git commit -m "Add version $aar_version" -m "Built off of $upstream_type $upstream_version from $upstream_repository"
